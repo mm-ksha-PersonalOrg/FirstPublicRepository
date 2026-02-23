d783590b758bf8806ad5bffcb8f8958d93662a1a
Kartavya Shukla <ksa@mm-software.com>
2026-02-23 15:54:00 +0530

    Clean up comments in MyClass.cs
    Removed unnecessary comments from MyClass.

--- MyProj/MyClass.cs
+++ MyProj/MyClass.cs
@@ -1,6 +1,4 @@
 // Adding comment for review testing
-// comment line is added to test PR Issue linking validation functionality
-// This is a simple class with two properties: MyInt and MyString.
 public class MyClass
 {
     public int MyInt { get; set; }
