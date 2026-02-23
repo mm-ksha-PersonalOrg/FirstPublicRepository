30f2bd8f5aa73754eaa0ce2dc860e0c45e6bea74
Kartavya Shukla <ksa@mm-software.com>
2026-02-23 15:40:03 +0530

    pushing changes to test validation on PR Issue linking (#28)

--- MyProj/MyClass.cs
+++ MyProj/MyClass.cs
@@ -1,4 +1,6 @@
 // Adding comment for review testing
+// comment line is added to test PR Issue linking validation functionality
+// This is a simple class with two properties: MyInt and MyString.
 public class MyClass
 {
     public int MyInt { get; set; }
